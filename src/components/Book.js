36857aafe85d46fd988a531ae6d0942c3ab8e232
ijeoma <ijeomaodinko@gmail.com>
2023-03-16 11:12:21 +0100

    Add updates events

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,7 +1,8 @@
 import './Book.css';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getBookThunk, removeBookThunk } from '../redux/books/books';
+import circularImage from './assets/circular-image.png';
 
 const Book = () => {
   const books = useSelector((state) => state.books);
@@ -15,6 +16,24 @@ const Book = () => {
   const clickHandle = (e) => {
     e.preventDefault();
     dispatch(removeBookThunk(e.target.id));
+  };
+
+  // to update progress
+  const [showProgress, setShowProgress] = useState(false);
+  const progressHandle = () => {
+    setShowProgress(true);
+  };
+
+  // to update the userss progress
+  const [currentChapter, setCurrentChapter] = useState(0);
+  const [totalChapters, setTotalChapter] = useState(0);
+  const [progress, setProgress] = useState(0);
+
+  const submitHandle = (event) => {
+    event.preventDefault();
+    const percentageProgress = Math.floor((currentChapter / totalChapters) * 100);
+    setProgress(percentageProgress);
+    setShowProgress(false);
   };
 
   // to render the page
@@ -40,16 +59,36 @@ const Book = () => {
             </div>
           </div>
           <div className="perDiv">
-            <p className="showPer">O</p>
+            <img src={circularImage} className="showPer" alt="percentrating" style={{ backgroundImage: `conic-gradient(#4386bf ${progress}%, #f7f7f7 ${progress}% 100%)` }} />
             <div className="per">
-              <h2 className="percent">64%</h2>
+              <h2 className="percent">
+                {progress}
+                %
+              </h2>
               <h3 className="perCompleted">Completed</h3>
             </div>
           </div>
           <div className="updateDiv">
             <h2 className="currentChapter">CURRENT CHAPTER</h2>
-            <h3 className="currentCha">Chapter 17</h3>
-            <button className="updateProgram" type="button">UPDATE PROGRESS</button>
+            <h3 className="currentCha">
+              Chapter
+              {currentChapter}
+            </h3>
+            <button className="updateProgram" type="button" onClick={progressHandle}>UPDATE PROGRESS</button>
+            {showProgress && (
+            <form onSubmit={submitHandle}>
+              <label htmlFor="progressForm" className="progressForm">
+                Current Chapter
+                <input type="number" id="progressForm" value={currentChapter} className="chapterForm" name="Current chapter" onChange={(event) => setCurrentChapter(event.target.value)} />
+              </label>
+              <br />
+              <label htmlFor="progressForm" className="progressForm">
+                Total Chapter
+                <input type="number" id="progressForm" value={totalChapters} className="chapterForm" name="Total chapter" onChange={(event) => setTotalChapter(event.target.value)} />
+              </label>
+              <button type="submit" className="updateBtn">Submit</button>
+            </form>
+            )}
           </div>
         </div>
       </section>
